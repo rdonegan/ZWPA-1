73f52bb21ff490b621a439534161a0eb62d6b2a2
Achal Channarasappa <achannar@andrew.cmu.edu>
2015-02-15 15:45:06 -0500

    initial commit

---
+++ zwpa_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZwpaApp::Application.config.secret_key_base = '761b6c0b613e24a869f9f0adae4ce38d234c36aead1c438a075d752cedd540b9428fa4e5fd582cdb70009759db5933ba10e046ad93757dfff1aa39d18aa1cb1e'
---
+++ zwpa_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZwpaApp::Application.config.session_store :cookie_store, key: '_zwpa_app_session'
