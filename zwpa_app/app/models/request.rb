c26fc64d4ba94201caa642ca7eeb958827254b80
Ryan Donegan <r.donegan44@gmail.com>
2015-02-22 22:46:12 -0500

    added validations for phone and email

--- zwpa_app/app/models/request.rb
+++ zwpa_app/app/models/request.rb
@@ -5,6 +5,9 @@ class Request < ActiveRecord::Base
 
 	# Validations
 	validates_presence_of :customer_id
+	validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "should be 10 digits (area code needed) and delimited with dashes only"
+  	validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, message: "is not a valid format"
+
 
 	#scopes
 	scope :chronological, -> { order('created_at') }
