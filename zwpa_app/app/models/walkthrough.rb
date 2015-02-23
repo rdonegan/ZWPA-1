79c2ed7154399a86b5bfccc6360bd664e82949e1
Maggie Li <maggiel@andrew.cmu.edu>
2015-02-22 22:16:36 -0500

    created relationships and validations for walkthrough model

--- zwpa_app/app/models/walkthrough.rb
+++ zwpa_app/app/models/walkthrough.rb
@@ -1,2 +1,12 @@
 class Walkthrough < ActiveRecord::Base
+	
+	# Relationships
+	belongs_to :request
+
+	# Validations
+	validates_presence_of :request_id
+	validates_numericality_of :square_footage, only_integer: true, greater_than_or_equal_to: 0
+	validates_numericality_of :num_floors, only_integer: true, greater_than_or_equal_to: 0
+	validates_numericality_of :max_occupancy, only_integer: true, greater_than_or_equal_to: 0
+
 end
