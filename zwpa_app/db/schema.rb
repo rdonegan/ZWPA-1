25f3694e6ad6ff98befea17dc0fcfc1a308e8f65
Achal Channarasappa <achannar@andrew.cmu.edu>
2015-02-22 21:06:27 -0500

    all migrations written and db migrated - schema.rb ready to go

--- zwpa_app/db/schema.rb
+++ zwpa_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223020147) do
+ActiveRecord::Schema.define(version: 20150223020409) do
 
   create_table "audits", force: true do |t|
     t.integer  "request_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150223020147) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "zip_code"
   end
 
   create_table "users", force: true do |t|
