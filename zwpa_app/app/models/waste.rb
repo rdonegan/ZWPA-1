1fc2fda2050671253daac089546de2dfd60c791a
Ryan Donegan <r.donegan44@gmail.com>
2015-02-23 22:05:06 -0500

    added relationships verification for waste model

--- zwpa_app/app/models/waste.rb
+++ zwpa_app/app/models/waste.rb
@@ -1,2 +1,15 @@
 class Waste < ActiveRecord::Base
+
+	#Relationships
+	belongs_to :audit
+
+
+	#Validations
+	validates_presence_of :audit_id, :material_type
+	validates_numericality_of :weight
+
+	#Scope
+	scope :chronological, -> { order('created_at') }
+
+
 end
