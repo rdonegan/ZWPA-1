29455e52893f5986b5ffc716d8d74979c5af90e7
Ryan Donegan <r.donegan44@gmail.com>
2015-02-24 16:21:25 -0500

    split up relationships for request over multiple lines

--- zwpa_app/app/models/request.rb
+++ zwpa_app/app/models/request.rb
@@ -1,7 +1,9 @@
 class Request < ActiveRecord::Base
 	# Relationships
 	belongs_to :customer
-	has_many :walkthroughs, :notes, :audits
+	has_many :walkthroughs
+	has_many :notes
+	has_many :audits
 
 	# Validations
 	validates_presence_of :customer_id, :date
