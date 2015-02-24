c99491ccc72d6f10ba4b80ddc57d9463385b76fd
Ryan Donegan <r.donegan44@gmail.com>
2015-02-23 20:52:00 -0500

    added relationships and validations to user model

--- zwpa_app/app/models/user.rb
+++ zwpa_app/app/models/user.rb
@@ -1,13 +1,33 @@
+# Refer to lab 13 from 272 for basic tutorials on how to use authentication.
+# These requirements are very involved, and should probably be saved more
+# for the end of the project.
+
+
 class User < ActiveRecord::Base
-	# Relationships
 	
+	# Adds method to set and authenticate against a Bcrypt password
+	# Uncomment this line later.
+	# has_secure_password
+
+	# Relationships
+	has_many :customers
 
 
 	# Validations
-
+	validates_presence_of :username
+	validates_presence_of :password
 
 	# Scopes
 
 
 	# Methods
+
+	# Used for user authentication via email.
+	# May have to changed based on future client needs.
+	# Uncomment when working on authentication later.
+	# def self.authenticate(email,password)
+	# 	find_by_email(email).try(:authenticate, password)
+	# end
+
+	#reset password - TO DO LATER
 end
