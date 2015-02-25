3ac040a04b5dac492fdf2636ccc781366596377d
Achal Channarasappa <achannar@andrew.cmu.edu>
2015-02-25 12:58:20 -0500

    added gems to setup testing environment

--- zwpa_app/test/models/note_test.rb
+++ zwpa_app/test/models/note_test.rb
@@ -4,4 +4,8 @@ class NoteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  should belong_to(:request)
+  should validate_presence_of(:request_id)
+
+  # test scope
 end
