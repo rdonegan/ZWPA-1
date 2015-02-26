6d58142089c919b85daf389c88c8bc2c36a20a85
Maggie Li <maggiel@andrew.cmu.edu>
2015-02-26 12:54:05 -0500

    US17 -wrote customers controller and added Gems-paginate and development

--- zwpa_app/app/controllers/customers_controller.rb
+++ zwpa_app/app/controllers/customers_controller.rb
@@ -1,34 +1,26 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
-  # GET /customers
-  # GET /customers.json
   def index
-    @customers = Customer.all
+    @customers = Customer.alphabetical.paginate(:page => params[:page]).per_page(10)
   end
 
-  # GET /customers/1
-  # GET /customers/1.json
   def show
   end
 
-  # GET /customers/new
   def new
     @customer = Customer.new
   end
 
-  # GET /customers/1/edit
   def edit
   end
 
-  # POST /customers
-  # POST /customers.json
   def create
     @customer = Customer.new(customer_params)
 
     respond_to do |format|
       if @customer.save
-        format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
+        format.html { redirect_to @customer, notice: "#{@customer.company_name} was added to the ZWPA system." }
         format.json { render action: 'show', status: :created, location: @customer }
       else
         format.html { render action: 'new' }
@@ -37,12 +29,10 @@ class CustomersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /customers/1
-  # PATCH/PUT /customers/1.json
   def update
     respond_to do |format|
       if @customer.update(customer_params)
-        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
+        format.html { redirect_to @customer, notice: "#{@customer.company_name} was successfully updated." }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -51,8 +41,6 @@ class CustomersController < ApplicationController
     end
   end
 
-  # DELETE /customers/1
-  # DELETE /customers/1.json
   def destroy
     @customer.destroy
     respond_to do |format|
