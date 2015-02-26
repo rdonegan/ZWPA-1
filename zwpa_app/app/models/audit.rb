4a871d15bd85f77714f5a7dab5760b9b153d9a63
Ryan Donegan <r.donegan44@gmail.com>
2015-02-25 23:50:16 -0500

    added validation for generator in audit model

--- zwpa_app/app/models/audit.rb
+++ zwpa_app/app/models/audit.rb
@@ -5,6 +5,7 @@ class Audit < ActiveRecord::Base
 
 	#Validations
 	validates_presence_of :request_id, :date, :generator, :location
+	validates_inclusion_of :generator, in: %w[Trash Recycle]
 	
 	#Need to validate that generator is within accepted values...
 
