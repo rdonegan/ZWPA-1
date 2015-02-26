5da57edfcad4f0b863a175fde4103f8bb41ff385
Achal Channarasappa <achannar@andrew.cmu.edu>
2015-02-26 00:52:13 -0500

    moved customer tests to contexts and sets

--- zwpa_app/test/models/customer_test.rb
+++ zwpa_app/test/models/customer_test.rb
@@ -16,14 +16,17 @@ class CustomerTest < ActiveSupport::TestCase
 
   context "Within context" do
 
+    setup do
+      create_customers
+    end
+
+    teardown do
+      delete_customers
+    end
+
     should "show that there are three customers in in alphabetical order" do
-      user1 = FactoryGirl.create(:user, username: "user", password: "secret")
-      c = FactoryGirl.create(:customer, user: user1, company_name: "Council")
-      a = FactoryGirl.create(:customer, user: user1, company_name: "Alcoa")
-      b = FactoryGirl.create(:customer, user: user1, company_name: "BurgerKing")
       assert_equal ["Alcoa", "BurgerKing", "Council"], Customer.alphabetical.all.map{|c| c.company_name}
     end
 
   end
-
 end
