8f6a6878ba671890469ce5b00e07ac125b6cc4cc
Ryan Donegan <r.donegan44@gmail.com>
2015-02-28 23:58:32 -0500

    added audits partial to home page

--- zwpa_app/app/controllers/home_controller.rb
+++ zwpa_app/app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@requests = Request.chronological.paginate(:page => params[:page], :per_page => 30)
+  	@requests = Request.chronological.paginate(:page => params[:page], :per_page => 15)
+  	@audits = Audit.chronological.paginate(:page => params[:page], :per_page => 20)
   end
 end
