a114c4ea4fa1bac6488b35c5b1adcfcffdb0f72d
Ryan Donegan <r.donegan44@gmail.com>
2015-03-10 16:55:05 -0400

    Added large image save option

--- zwpa_app/app/models/note.rb
+++ zwpa_app/app/models/note.rb
@@ -2,7 +2,7 @@ class Note < ActiveRecord::Base
 
 	#Relationships
 	belongs_to :request
-	has_attached_file :image, styles: {normal: "300x300>", thumb: "150x150#"}
+	has_attached_file :image, styles: {large: "500x500>", normal: "300x300>", thumb: "150x150#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 	#Validations
