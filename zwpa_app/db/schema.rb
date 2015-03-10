e12f55585b4518d8459a41081020bf4f932e6ff7
Ryan Donegan <r.donegan44@gmail.com>
2015-03-10 16:37:56 -0400

    added paperclip gem and image attachment to note. Migrated database

--- zwpa_app/db/schema.rb
+++ zwpa_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226044903) do
+ActiveRecord::Schema.define(version: 20150310203620) do
 
   create_table "audits", force: true do |t|
     t.integer  "request_id"
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(version: 20150226044903) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "requests", force: true do |t|
