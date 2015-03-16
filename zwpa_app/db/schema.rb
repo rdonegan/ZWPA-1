35aee4feb4423276556c794f20bd10f9e18b7643
Achal Channarasappa <achannar@andrew.cmu.edu>
2015-03-16 14:44:30 -0400

    successfully changed to postgres db

--- zwpa_app/db/schema.rb
+++ zwpa_app/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150314215325) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "audits", force: true do |t|
     t.integer  "request_id"
     t.date     "date"
