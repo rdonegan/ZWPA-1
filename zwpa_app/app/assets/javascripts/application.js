10c2042fdf67c3c8a3b98fa8d251a8989b10a8fa
Ryan Donegan <r.donegan44@gmail.com>
2015-03-17 10:44:44 -0400

    added datepicker to new requests page

--- zwpa_app/app/assets/javascripts/application.js
+++ zwpa_app/app/assets/javascripts/application.js
@@ -26,6 +26,7 @@ $(function() {
   $(".datepicker").datepicker({
   	changeMonth: true,
     changeYear: true,
-    format: 'mm/dd/YYYY'
+    // format: 'mm/dd/YYYY'
+    dateFormat: "yyyy-mm-dd"
   });
 });
