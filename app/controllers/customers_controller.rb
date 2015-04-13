e5ceccd6ca24deec7c130ac7c9be7d2d4f8ee2ca
Ryan Donegan <r.donegan44@gmail.com>
2015-04-12 21:10:47 -0400

    fixed some formatting for index and show pages

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,7 +6,7 @@ class CustomersController < ApplicationController
   end
 
   def show
-    @requests = @customer.requests.to_a
+    @requests = @customer.requests.paginate(:page => params[:page]).per_page(10)
   end
 
   def new
