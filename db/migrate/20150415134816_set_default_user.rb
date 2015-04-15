08218599fea14d1d269a4372a09d01809c84ec0e
Achal Channarasappa <achannar@andrew.cmu.edu>
2015-04-15 09:56:59 -0400

    changed user schema and set default user

--- db/migrate/20150415134816_set_default_user.rb
+++ db/migrate/20150415134816_set_default_user.rb
@@ -1,14 +1,20 @@
 class SetDefaultUser < ActiveRecord::Migration
-  def up
-    zwpa = User.new
-    zwpa.username = "zwpa"
-    zwpa.password = "sixty4S14th"
-    zwpa.role = "admin"
-    zwpa.save!
-  end
+	def change
+		remove_column :users, :password
+		add_column :users, :password_digest, :string
+		add_column :users, :role, :string
+	end
 
-  def down
-    zwpa = User.last
-    User.delete zwpa
-  end
+	def up
+	    zwpa = User.new
+	    zwpa.username = "zwpa"
+	    zwpa.password = "secret"
+	    zwpa.role = "admin"
+	    zwpa.save!
+	end
+
+    def down
+    	zwpa = User.last
+    	User.delete zwpa
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414194023) do
+ActiveRecord::Schema.define(version: 20150415134816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,9 +72,10 @@ ActiveRecord::Schema.define(version: 20150414194023) do
 
   create_table "users", force: true do |t|
     t.string   "username"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "role"
   end
 
   create_table "walkthroughs", force: true do |t|
