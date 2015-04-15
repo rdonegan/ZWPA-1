2b5770894bac2d5d0c35887a2c6c4ae3ab0bbb5b
Achal Channarasappa <achannar@andrew.cmu.edu>
2015-04-15 11:02:16 -0400

    added commented code to handle error page. need to complete later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # TODO: create a standardized error page with our styling
+  # rescue_from CanCan::AccessDenied do |exception|
+  #   # flash[:error] = "You are not authorized to access this page. Please login before continuing"
+  #   # render template: 'errors/unauthorized'
+  # end
+
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
