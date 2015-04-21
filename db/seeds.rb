863c48ade787798eca06691283d84762bd9ad53a
Achal Channarasappa <achannar@andrew.cmu.edu>
2015-04-21 11:39:08 -0400

    updated username and password to what Ross said. also now create the user in db/seeds.rb instead of in a db migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create the one and only system user - as requested by the client
+User.create({username: "zerowaste@prc.org", password: "recycle1", password_confirmation: "recycle1", role: "admin" })
