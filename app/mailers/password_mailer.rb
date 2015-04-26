e29b6b9a619d64de0954c691d9fc358ddd837a0b
Achal Channarasappa <achannar@andrew.cmu.edu>
2015-04-26 14:34:41 -0400

    password mailer works!

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -1,3 +1,8 @@
 class PasswordMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "zwpawasteaudit@gmail.com"
+
+  def password_email
+  	# change this to zerowaste@prc.org after testing
+  	mail(to: 'achannar@andrew.cmu.edu', subject: "zwpawasteaudit.org Password Recovery")  	
+  end
 end
